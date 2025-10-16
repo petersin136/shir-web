563acf8d0fb36a8819fe7bd673bc7c82c2029498
petersin136 <petersin136@gmail.com>
2025-10-16 18:45:12 +0900

    사역초청수정

--- middleware.ts
+++ middleware.ts
@@ -7,8 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    // Restrict middleware to protected sections only
-    "/protected/:path*",
-    "/manage/:path*",
+    // 일단 middleware 비활성화
   ],
 };
